6c3de19387f1f67ef70d41b6dc25a730335500b7
Bitt.js <118621206+Bittjs@users.noreply.github.com>
2024-02-23 13:05:15 +0700

    yooooo

---
+++ test.js
@@ -0,0 +1,6 @@
+function submitData(){
+    const theLogin = document.getElementById("loginSpace").value
+    const thePassword = document.getElementById("passwordSpace").value
+
+    console.log(theLogin, "and", thePassword)
+}
